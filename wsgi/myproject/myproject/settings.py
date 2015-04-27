3e346816ee86a8352076639a69abb633bdfa1e50
Jonathan Anderson <jonathan@jonathananderson.se>
2015-04-27 11:22:11 +0200

    Beta 1.2 debug

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 SECRET_KEY = '!iudke*hi8vo#qyntq5yxm+p2itkuqg-m@bo8o%+cbnq(h%@@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
